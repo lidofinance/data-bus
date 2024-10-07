fdf19093c9d6677cfecfc0dd6f17c2b4d6f57331
Eddort <webisforce@gmail.com>
2024-10-07 16:54:42 +0200

    feat: preparation for deploy

--- deploy/fork.sh
+++ deploy/fork.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script checks the contract deployment on all networks, as well as the functionality of public node.
+# If any of the nodes are not giving the correct data, it is worth finding a new node on the site https://chainlist.org/ before deploying. 
+
 FORKING_NETWORK=gnosis npx hardhat ignition deploy ignition/modules/data-bus.ts --strategy create2
 FORKING_NETWORK=polygon npx hardhat ignition deploy ignition/modules/data-bus.ts --strategy create2
 FORKING_NETWORK=base-mainnet npx hardhat ignition deploy ignition/modules/data-bus.ts --strategy create2
--- deploy/network.sh
+++ deploy/network.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-npx hardhat ignition deploy ignition/modules/data-bus.ts --network gnosis --strategy create2 --verify
-npx hardhat ignition deploy ignition/modules/data-bus.ts --network polygon --strategy create2 --verify
-npx hardhat ignition deploy ignition/modules/data-bus.ts --network base-mainnet --strategy create2 --verify
-npx hardhat ignition deploy ignition/modules/data-bus.ts --network optimism --strategy create2 --verify
-npx hardhat ignition deploy ignition/modules/data-bus.ts --network chiado --strategy create2 --verify
+# It is recommended to uncomment and run one by one, writing out addresses, verification status
+
+# npx hardhat ignition deploy ignition/modules/data-bus.ts --network chiado --strategy create2 --verify
+
+# npx hardhat ignition deploy ignition/modules/data-bus.ts --network gnosis --strategy create2 --verify
+
+# npx hardhat ignition deploy ignition/modules/data-bus.ts --network polygon --strategy create2 --verify
+
+# npx hardhat ignition deploy ignition/modules/data-bus.ts --network base-mainnet --strategy create2 --verify
+
+# npx hardhat ignition deploy ignition/modules/data-bus.ts --network optimism --strategy create2 --verify
