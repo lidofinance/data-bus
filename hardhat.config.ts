1afd35447376aa94ffe021722159daa753ca8500
Eddort <webisforce@gmail.com>
2024-10-07 16:31:57 +0200

    feat: create2 deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
-import { HardhatUserConfig } from "hardhat/types";
+
+import { parseContractEnvConfig } from "./lib/config";
 
 import "@nomicfoundation/hardhat-verify";
 import "@nomicfoundation/hardhat-ignition-ethers";
@@ -6,48 +7,45 @@ import "@nomicfoundation/hardhat-ethers/types";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-chai-matchers";
 
-import { parseContractEnvConfig } from "./lib/config";
-import { encodeBytes32String } from "ethers";
 
-// accountsPlugin(["gnosis", "chiado"]);
+import { encodeBytes32String } from "ethers";
+import { addForking } from "./lib/add-forking";
 
 const envConfig = parseContractEnvConfig(process.env);
+
+const baseNetworks = {
+  gnosis: {
+    url: "https://rpc.gnosischain.com",
+    accounts: [envConfig.PK_KEY],
+  },
+  chiado: {
+    url: "https://gnosis-chiado-rpc.publicnode.com",
+    gasPrice: 1000000000,
+    accounts: [envConfig.PK_KEY],
+  },
+  optimism: {
+    url: "https://optimism-rpc.publicnode.com",
+    gasPrice: 1000000000,
+    accounts: [envConfig.PK_KEY],
+  },
+  polygon: {
+    url: "https://polygon-bor-rpc.publicnode.com",
+    gasPrice: 1000000000,
+    accounts: [envConfig.PK_KEY],
+  },
+  "base-mainnet": {
+    url: "https://mainnet.base.org",
+    gasPrice: 1000000000,
+    accounts: [envConfig.PK_KEY],
+  },
+};
 
 const config: any = {
   solidity: "0.8.26",
   //   defaultNetwork: 'local',
   networks: {
-    gnosis: {
-      // https://gnosis-rpc.publicnode.com
-      url: "https://rpc.gnosischain.com",
-      accounts: [envConfig.PK_KEY],
-    },
-    // local: {
-    //   url: envConfig.NODE_HOST,
-    // },
-    chiado: {
-      url: "https://gnosis-chiado-rpc.publicnode.com",
-      gasPrice: 1000000000,
-      accounts: [envConfig.PK_KEY],
-    },
-
-    optimism: {
-      url: "https://optimism-rpc.publicnode.com",
-      gasPrice: 1000000000,
-      accounts: [envConfig.PK_KEY],
-    },
-
-    polygon: {
-      url: "https://polygon-bor-rpc.publicnode.com",
-      gasPrice: 1000000000,
-      accounts: [envConfig.PK_KEY],
-    },
-
-    'base-mainnet': {
-      url: 'https://mainnet.base.org',
-      accounts: [process.env.WALLET_KEY as string],
-      gasPrice: 1000000000,
-    },
+    ...baseNetworks,
+    hardhat: addForking({}, baseNetworks),
   },
   etherscan: {
     customChains: [
@@ -69,39 +67,38 @@ const config: any = {
           //apiURL: "https://blockscout.com/xdai/mainnet/api",
           //browserURL: "https://blockscout.com/xdai/mainnet",
         },
-        
       },
       {
         network: "optimism",
         chainId: 10,
         urls: {
           apiURL: "https://api.optimistic.etherscan.io/api",
-          browserURL: "https://optimistic.etherscan.io"
-        }
+          browserURL: "https://optimistic.etherscan.io",
+        },
       },
       {
         network: "polygon",
         chainId: 137,
         urls: {
           apiURL: "https://api.polygonscan.com/api",
-          browserURL: "https://polygonscan.com"
-        }
+          browserURL: "https://polygonscan.com",
+        },
       },
       {
         network: "base-mainnet",
         chainId: 8453,
         urls: {
-         apiURL: "https://api.basescan.org/api",
-         browserURL: "https://basescan.org"
-        }
-      }
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org",
+        },
+      },
     ],
     apiKey: {
       chiado: envConfig.CHIADO_BLOCKSCOUT,
       gnosis: envConfig.GNOSISSCAN,
       "base-mainnet": envConfig.BASESCAN,
       optimism: envConfig.OPTIMISTICSCAN,
-      polygon: envConfig.POLYGONSCAN
+      polygon: envConfig.POLYGONSCAN,
     },
   },
   mocha: {
@@ -111,7 +108,7 @@ const config: any = {
     strategyConfig: {
       create2: {
         // To learn more about salts, see the CreateX documentation
-        salt: encodeBytes32String('data bus deploy 1.0'),
+        salt: encodeBytes32String("data bus deploy 1.0"),
       },
     },
   },
--- lib/config.ts
+++ lib/config.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const EnvContractSchema = z.object({
+  FORKING_NETWORK: z.string().optional(),
   CHIADO_BLOCKSCOUT: z.string(),
   GNOSISSCAN: z.string(),
   POLYGONSCAN: z.string(),
@@ -24,13 +25,14 @@ export type EnvMonitoring = z.infer<typeof EnvMonitoringSchema>;
 
 export const parseContractEnvConfig = (env: NodeJS.ProcessEnv): EnvContract =>
   EnvContractSchema.parse({
+    FORKING_NETWORK: env.FORKING_NETWORK,
     CHIADO_BLOCKSCOUT: env.CHIADO_BLOCKSCOUT,
     GNOSISSCAN: env.GNOSISSCAN,
     POLYGONSCAN: env.POLYGONSCAN,
     OPTIMISTICSCAN: env.OPTIMISTICSCAN,
     BASESCAN: env.BASESCAN,
     NODE_HOST: env.NODE_HOST,
-    PK_KEY: env.PK_KEY
+    PK_KEY: env.PK_KEY,
   });
 
 export const parseMonitoringConfig = (env: NodeJS.ProcessEnv): EnvMonitoring =>
